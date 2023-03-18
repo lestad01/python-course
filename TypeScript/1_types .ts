fc1efa8a939a2b23178c827c237406d65d14c444
Aleksandr <Lestad01@mail.ru>
2023-03-18 11:43:55 +0300

    Type Script 18.03.23

---
+++ TypeScript/1_types .ts
@@ -0,0 +1,24 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
+
+const message: string = 'Hello TypeScript';
+// Массив
+const numberArray: number[] = [1,2,3,4,5];
+// джинерик
+const numberArray2: Array<number> = [1,2,3,4];
+
+const words: string[] = ['Hello', 'TypeScript'];
+
+// Tuple массив из разных типов данныъ
+const contact: [string,number] = ['Vladiken', 1234124];
+
+// для присвоения переменной ЛЮБОГО типа данных используем Any
+let variable: any = 42;
+variable = 'New String';
+
+
+
