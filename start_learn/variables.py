c56ebf05a46669b6b4cd3d6ed4e150eea6799b32
Aleksandr <Lestad01@mail.ru>
2023-09-15 10:19:07 +0300

    15.09.23

---
+++ start_learn/variables.py
@@ -0,0 +1,31 @@
+number = 3
+number2 = 5
+result = number2 + number
+print(result)
+
+
+num1 = num2 = 5
+print(num1,num2)
+
+num_1,num_2 = 22, 9
+print(num_1,num_2)
+
+# swap data - обмен данными
+swap1 = 8
+swap2 = 9
+#swap1,swap2 = swap2, swap1
+swap1,swap2 = swap2, swap1 + swap2
+print(swap1,swap2)
+
+swap2 -= number2
+print(swap2)
+
+
+# распаковка данных из списка
+z,*x,c = [1,2,3,4,5]
+print(z)
+print(x)
+print(c)
+
+
+
