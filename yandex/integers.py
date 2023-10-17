fd63120698da291d20d344f5d6f7ae4d89d89b86
Aleksandr <Lestad01@mail.ru>
2023-10-17 19:42:14 +0300

    17.10.23

--- yandex/integers.py
+++ yandex/integers.py
@@ -6,9 +6,9 @@
 # print(type(input_int))
 
 
-pow_number = pow(2, 3)
-print(pow_number)
-print(type(pow_number))
+# pow_number = pow(2, 3)
+# print(pow_number)
+# print(type(pow_number))
 
 
 long_int = 1_000_000_000
@@ -19,3 +19,23 @@ bool_val = True
 int_value = 10
 
 print(bool_val + int_value)
+
+# магические методы
+
+int_num = 50
+float_num = 7.5
+str_val = 'abc'
+
+# print(str_val * int_num)
+
+print(int_num.__mul__(str_val))
+# NotImplemented
+
+print(str_val.__rmul__(int_num))
+
+# print(int_num + float_num)
+
+# print(int_num.__mul__(float_num))
+# NotImplemented
+
+# print(float_num.__rmul__(int_num))
