89b64da269aef08b878cce82162967b2718e260c
lestad01 <lestad01@mail.ru>
2023-10-18 22:06:59 +0300

    end lection 18.10.23

--- python-course/yandex/copy_lists.py
+++ python-course/yandex/copy_lists.py
@@ -26,3 +26,24 @@ another_copied_cars = my_cars.copy()
 another_copied_cars.append('Folkswagen')
 
 print(id(my_cars) == id(another_copied_cars))
+
+
+# 3 вариант list создает новый список
+copy_cars = my_cars.copy()
+copy_cars.append('Audi')
+
+print(id(my_cars) == id(copy_cars))
+
+
+my_nums = [1, 1, 6, 3, 21]
+print(my_nums.count(1))
+
+
+my_nums.insert(2, -9)
+print(my_nums)
+
+my_nums.extend('123')
+print(my_nums)
+
+my_nums.clear()
+print(my_nums)
