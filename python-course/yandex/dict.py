72e8bcb507325b2ff389a4b8f167bd48308656e0
lestad01 <lestad01@mail.ru>
2023-10-25 23:27:32 +0300

    add new file dict.py

--- python-course/yandex/dict.py
+++ python-course/yandex/dict.py
@@ -1,1 +1,26 @@
 # словарь набор данных с типом ключ:значение
+my_motorbike = {
+    'brand': 'Ducati',
+    'price': 25000,
+    'engine_vol': 1.2
+}
+# порядок элементов в словаре не имеет значения
+# индексо у элементов в словаре нет
+other_motorbike = {
+    'engine_vol': 1.2,
+    'price': 25000,
+    'brand': 'Ducati',
+}
+print(id(my_motorbike) == id(other_motorbike))
+print(my_motorbike == other_motorbike)
+
+my_motorbike['price'] = 6000
+print(my_motorbike)
+
+print(other_motorbike['brand'])
+# print(other_motorbike[brand])
+my_motorbike['is_new_key'] = True
+print(my_motorbike)
+
+del my_motorbike['is_new_key']
+print(my_motorbike)
